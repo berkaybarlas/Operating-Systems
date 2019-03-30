6ecf11009012a428ded0c53a32fad9790ab414e8
Berkay Barlas <fpberkay@gmail.com>
2019-03-30 13:32:28 +0300

    kernel module child process start_times

--- Project-1/oldestChild.c
+++ Project-1/oldestChild.c
@@ -34,32 +34,21 @@ static void custom_module_exit(void) {
 
 void oldestchild(struct task_struct *t){
 	//If no process ID is provided or the process ID is invalid, print an error message to kernel log.
-
+		
     for_each_process(task) {
 		if (task->pid == processID){
 			printk(KERN_INFO "PID: [%d]\n", task->pid);
 			printk(KERN_INFO "Parent PID: [%d]\n", (task->parent)->pid);
+			printk(KERN_INFO "Process Start time: [%d]\n", task->start_time);
 			printk(KERN_INFO "Executable Name: [%s]",task->comm);
 
 			list_for_each(list, &current->children) {
 				temp_task = list_entry(list, struct task_struct, sibling);
 				printk(KERN_INFO "\tChild PID: [%d]\n",temp_task->pid);
+				printk(KERN_INFO "\tChild Start time: [%d]\n", temp_task->start_time);
 				printk(KERN_INFO "\tChild Executable Name: [%s]\n",temp_task->comm);
 			}
 
-			list_for_each(list2,&task->thread_group){
-				temp_task2 = list_entry(list2, struct task_struct, sibling);
-				printk(KERN_INFO "\tThread Group List Member PID: [%d]\n",temp_task2->pid);
-				printk(KERN_INFO "\tThread Group List Member Name: [%s]\n",temp_task2->comm);
-			}
-
-			printk(KERN_INFO "user ID: %d\n",task->cred->uid);
-			printk(KERN_INFO "Process Voluntary Context Switch Count: %lu\n",task->nvcsw);
-			printk(KERN_INFO "Process Involuntary Context Switch Count: %lu\n",task->nivcsw);
-			printk(KERN_INFO "Priority: %d\n",task->prio);
-			printk(KERN_INFO "vruntime: %llu\n",(task->se).vruntime);
-			task->prio = processPrio;
-			printk(KERN_INFO "Updated Priority: %d\n",task->prio);
 		}
 	}
 }
