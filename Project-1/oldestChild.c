1ae25b9b5ee5bbeca5c0ef17c723d62b9e9a2ba1
egeonat <egeonat@gmail.com>
2019-03-30 16:06:05 +0300

    Fixes

--- Project-1/oldestChild.c
+++ Project-1/oldestChild.c
@@ -13,6 +13,7 @@ KUSIS ID: 54512 PARTNER NAME: Berkay Barlas
 #include <linux/slab.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
+#include <linux/pid.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Berkay Barlas, Ege Onat Ozsuer");
@@ -26,7 +27,16 @@ void oldestchild(struct task_struct *t);
 static int custom_module_init(void) {
 		printk(KERN_INFO "Loading Module\n");
 		printk(KERN_INFO "Root processID:%d\n",processID);
-		oldestchild(current);
+		struct pid *pid_struct;
+		struct task_struct *task;
+		
+		pid_struct = find_get_pid(processID);
+		task = pid_task(pid_struct, PIDTYPE_PID);
+		printk(KERN_INFO "PID: [%d]\n", task->pid);
+		printk(KERN_INFO "Parent PID: [%d]\n", (task->parent)->pid);
+		printk(KERN_INFO "Process Start time: [%d]\n", task->start_time);
+		printk(KERN_INFO "Executable Name: [%s]",task->comm);
+		oldestchild(task);
 		return 0;
 }
 
@@ -40,24 +50,24 @@ void oldestchild(struct task_struct *t) {
 		struct task_struct *task = t;
 		struct task_struct *temp_task;
 		struct list_head *list;
-		
-    for_each_process(task) {
-		if (task->pid == processID){
-			printk(KERN_INFO "PID: [%d]\n", task->pid);
-			printk(KERN_INFO "Parent PID: [%d]\n", (task->parent)->pid);
-			printk(KERN_INFO "Process Start time: [%d]\n", task->start_time);
-			printk(KERN_INFO "Executable Name: [%s]",task->comm);
-
-			list_for_each(list, task->children) {
+	
+	
+    //for_each_process(task) 
+    {
+		//if (task->pid == processID)
+		{
+			list_for_each(list, &task->children) {
 				temp_task = list_entry(list, struct task_struct, sibling);
+				oldestchild(temp_task);
 				printk(KERN_INFO "\tChild PID: [%d]\n",temp_task->pid);
 				printk(KERN_INFO "\tChild Start time: [%d]\n", temp_task->start_time);
 				printk(KERN_INFO "\tChild Executable Name: [%s]\n",temp_task->comm);
 			}
-
 		}
 	}
 }
+
+
 
 module_init(custom_module_init);
 module_exit(custom_module_exit);
