ce386746dd638240db4576457d3e888d0eca9ed9
Berkay Barlas <fpberkay@gmail.com>
2019-03-30 15:02:07 +0300

    update kernel

--- Project-1/oldestChild.c
+++ Project-1/oldestChild.c
@@ -20,9 +20,9 @@ int processID = 0;
 module_param(processID,int,0);
 
 void print_details(struct task_struct *t);
-void oldestchild(struct task_struct *t)
+void oldestchild(struct task_struct *t);
 
-static int custom_module_init(void){
+static int custom_module_init(void) {
 		printk(KERN_INFO "Loading Module\n");
 		printk(KERN_INFO "Root processID:%d\n",processID);
 		oldestchild(current);
@@ -33,9 +33,11 @@ static void custom_module_exit(void) {
 	printk(KERN_INFO "Removing Module...\n");
 }
 
-void oldestchild(struct task_struct *t){
+void oldestchild(struct task_struct *t) {
 	//If no process ID is provided or the process ID is invalid, print an error message to kernel log.
-		
+		struct task_struct *task = t;
+		struct task_struct *temp_task;
+
     for_each_process(task) {
 		if (task->pid == processID){
 			printk(KERN_INFO "PID: [%d]\n", task->pid);
@@ -43,7 +45,7 @@ void oldestchild(struct task_struct *t){
 			printk(KERN_INFO "Process Start time: [%d]\n", task->start_time);
 			printk(KERN_INFO "Executable Name: [%s]",task->comm);
 
-			list_for_each(list, &current->children) {
+			list_for_each(list, task->children) {
 				temp_task = list_entry(list, struct task_struct, sibling);
 				printk(KERN_INFO "\tChild PID: [%d]\n",temp_task->pid);
 				printk(KERN_INFO "\tChild Start time: [%d]\n", temp_task->start_time);
