a17f7f43e5f68529b4bd6f17c97ad10508996c50
Berkay Barlas <fpberkay@gmail.com>
2019-04-25 14:26:22 +0300

    started project-2

---
+++ Project-2/pthread_sleep.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+ /****************************************************************************** 
+  pthread_sleep takes an integer number of seconds to pause the current thread 
+  original by Yingwu Zhu
+  updated by Muhammed Nufail Farooqi
+  *****************************************************************************/
+int pthread_sleep (int seconds)
+{
+   pthread_mutex_t mutex;
+   pthread_cond_t conditionvar;
+   struct timespec timetoexpire;
+   if(pthread_mutex_init(&mutex,NULL))
+    {
+      return -1;
+    }
+   if(pthread_cond_init(&conditionvar,NULL))
+    {
+      return -1;
+    }
+   struct timeval tp;
+   //When to expire is an absolute time, so get the current time and add //it to our delay time
+   gettimeofday(&tp, NULL);
+   timetoexpire.tv_sec = tp.tv_sec + seconds; timetoexpire.tv_nsec = tp.tv_usec * 1000;
+
+   pthread_mutex_lock (&mutex);
+   int res =  pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
+   pthread_mutex_unlock (&mutex);
+   pthread_mutex_destroy(&mutex);
+   pthread_cond_destroy(&conditionvar);
+
+   //Upon successful completion, a value of zero shall be returned
+   return res;
+
+}
+
