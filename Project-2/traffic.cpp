4696cb5982338bde488eaf3c1bc2e5fab6aa4eda
Berkay Barlas <fpberkay@gmail.com>
2019-04-25 15:02:54 +0300

    update makefile

--- Project-2/traffic.cpp
+++ Project-2/traffic.cpp
@@ -6,21 +6,32 @@ using namespace std;
 
 #define NUM_THREADS 5
 
-void *PrintHello(void *threadid) {
-   long tid;
-   tid = (long)threadid;
-   cout << "Hello World! Thread ID, " << tid << endl;
+struct thread_data {
+   int  thread_id;
+   char *message;
+};
+
+void *PrintHello(void *threadarg) {
+   struct thread_data *my_data;
+   my_data = (struct thread_data *) threadarg;
+
+   cout << "Thread ID : " << my_data->thread_id ;
+   cout << " Message : " << my_data->message << endl;
+
    pthread_exit(NULL);
 }
 
 int main () {
    pthread_t threads[NUM_THREADS];
+   struct thread_data td[NUM_THREADS];
    int rc;
    int i;
-   
+
    for( i = 0; i < NUM_THREADS; i++ ) {
-      cout << "main() : creating thread, " << i << endl;
-      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+      cout <<"main() : creating thread, " << i << endl;
+      td[i].thread_id = i;
+      td[i].message = "This is message";
+      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
       
       if (rc) {
          cout << "Error:unable to create thread," << rc << endl;
